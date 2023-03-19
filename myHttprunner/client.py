d98c9bfeb845eedb20c40a38a8db8d4693ed1df3
SEtester <460060639@qq.com>
2023-03-20 00:03:21 +0800

    用于测试框架封装的接口服务

--- myHttprunner/client.py
+++ myHttprunner/client.py
@@ -1,16 +1,22 @@
 import requests
 
-client = requests.session()
-client.request()
 
 class HttpClient():
 
-    def send_response(self):
-        pass
+    def __init__(self, request_desc, client=None):
+        self.request_desc = request_desc
+        self.client = client
 
+    def send_response(self):
+        if not self.client:
+            self.client = requests.Session()
+        resp = self.client.request(**self.request_desc)
+        return resp
 
     def run(self):
+        # 组装请求
+        # 前置脚本
+        # 发送http请求
+        # 后置脚本
+        # 断言
         pass
-
-
-
